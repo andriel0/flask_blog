5a9c5620bced95bf4ee8c4bff809902ef026cdae
Andriel Alexandre <an-driel@hotmail.com>
2022-10-22 10:53:34 -0300

    day 3

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
 lista_usuarios = ['andriel', 'alexandre', 'oliveira']
 
+app.config['SECRET_KEY'] = 'e3af64a50713936d82691a3739ec6bb9'
 
 @app.route("/")
 def home():
@@ -17,9 +19,18 @@ def contatos():
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criar_conta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'btn_submit_login' in request.form:
+        flash(f'Login feito com sucesso no e-mail {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if form_criar_conta.validate_on_submit() and 'btn_submit_criar_conta' in request.form:
+        flash(f'Conta criada com sucesso no e-mail {form_criar_conta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta)
 
 
 if __name__ == '__main__':
