c338b1460dabb80f0ab2bda2c9467831c2148f24
Andriel Alexandre <an-driel@hotmail.com>
2022-10-23 13:40:54 -0300

    day 4

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 lista_usuarios = ['andriel', 'alexandre', 'oliveira']
 
 app.config['SECRET_KEY'] = 'e3af64a50713936d82691a3739ec6bb9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
 
 @app.route("/")
 def home():
