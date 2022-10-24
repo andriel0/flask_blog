ea99befa56044b44a720fcda7ca6c3d8104f2f11
Andriel Alexandre <an-driel@hotmail.com>
2022-10-24 00:10:16 -0300

    day 4

--- models.py
+++ models.py
@@ -8,9 +8,13 @@ class Usuario(database.Model):
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, default='default.jpg', nullable=False)
+    posts = database.relationship('Post', backref='autor', lazy=True)
+    cursos = database.Column(database.String, default='Não informado', nullable=False)
+
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     titulo = database.Column(database.String, nullable=False)
     corpo = database.Column(database.Text, nullable=False)
     data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = database.Column(database.Integer, database.ForeigeKey('usuario.id'), nullable=False)
