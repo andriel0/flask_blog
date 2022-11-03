2dd827229c00f52fe52dba77afba9d2f02232926
Andriel Alexandre <an-driel@hotmail.com>
2022-11-03 00:25:51 -0300

    day 9

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadeimpressionadora.models import Usuario
+from flask_login import current_user
 
 
 class FormCriarConta(FlaskForm):
@@ -14,8 +16,7 @@ class FormCriarConta(FlaskForm):
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            msg = 'Email já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar.'
-            raise ValidationError(msg)
+            raise ValidationError('Email já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar.')
 
 
 class FormLogin(FlaskForm):
@@ -28,6 +29,18 @@ class FormLogin(FlaskForm):
 class FormEditarPerfil(FlaskForm):
     username = StringField('Nome do Usuário', validators=[DataRequired(), Length(5, 20)])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar foto de perfil', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    curso_excel = BooleanField("Excel Impressionador")
+    curso_vba = BooleanField("VBA Impressionador")
+    curso_powerbi = BooleanField("Power BI Impressionador")
+    curso_python = BooleanField("Python Impressionador")
+    curso_ppt = BooleanField("Power Point Impressionador")
+    curso_sql = BooleanField("SQL Impressionador")
     btn_submit_editar = SubmitField('Confirmar edição')
 
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe outro usuário com esse email, cadastre outro email.')
 
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -3,6 +3,9 @@ from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEdita
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.models import Usuario, Post
 from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
+from PIL import Image
 
 lista_usuarios = ['andriel', 'alexandre', 'oliveira']
 
@@ -73,9 +76,38 @@ def perfil():
 def criar_post():
     return render_template('criarpost.html')
 
+
+def salvar_imagem(imagem):
+    # criptografando e colocando na pasta
+    codigo = secrets.token_hex(8)
+    nome, extensao = os.path.splitext(imagem.filename)
+    nome_arquivo = nome + codigo + extensao
+    caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo)
+
+    # reduzindo tamanho da foto
+    tamanho = (400, 400)
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+
+    # salvando a foto reduzida
+    imagem_reduzida.save(caminho_completo)
+    return nome_arquivo
+
 @app.route("/perfil/editar", methods=['GET', 'POST'])
 @login_required
 def editar_perfil():
     form = FormEditarPerfil()
+    if form.validate_on_submit():
+        current_user.email = form.email.data
+        current_user.username = form.username.data
+        if form.foto_perfil.data:
+            imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil = imagem
+        database.session.commit()
+        flash(f'Perfil atualizado com sucesso', 'alert-success')
+        return redirect(url_for('perfil'))
+    elif request.method == 'GET':
+        form.email.data = current_user.email
+        form.username.data = current_user.username
     foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('editar_perfil.html', foto_perfil=foto_perfil, form=form)
